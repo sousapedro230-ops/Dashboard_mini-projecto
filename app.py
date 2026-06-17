fd455bed5bcdb8d169831728f3e8351a1fe23dc6
sousapedro230-ops <sousa.pedro230@gmail.com>
2026-06-17 19:32:43 +0000

    Alternação e adição de novo csv

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import pandas as pd
 # Configuração da página
 # --------------------------------------------------
 st.set_page_config(
-    page_title="Superstore Dashboard", # O título que mostra na tab do browser
+    page_title="Players Dashboard", # O título que mostra na tab do browser
     layout="wide" # A opção "centered" coloca a página numa coluna central
 )
 
-st.title("📊 Superstore Dashboard")
-st.markdown("Dashboard de vendas")
+st.title("📊 Players Dashboard")
+st.markdown("Dashboard de Jogadores")
 
 # --------------------------------------------------
 # Carregamento dos dados
@@ -20,121 +20,120 @@ st.markdown("Dashboard de vendas")
 # Assim, sempre que houver interações com o dashboard (ex: mudar um filtro), não é necessário ler o ficheiro csv novamente
 @st.cache_data 
 def load_data():
-    file_name = "Superstore.csv"
-    df = pd.read_csv(file_name, parse_dates=["Order Date"])
+    file_name = "players_data-2025_2026.csv"
+    df = pd.read_csv(["players_data-2025_2026.csv"])
     return df
 
-df = load_data()
 
 # --------------------------------------------------
 # Definir um Sidebar com filtros
 # --------------------------------------------------
 st.sidebar.header("Filtros")
 
-# Filtro de Região
-sorted_regions = sorted(df["Region"].unique())
-regions = st.sidebar.multiselect(
-    "Região",
-    options=sorted_regions,
-    default=sorted_regions
+# Filtro de Nações
+sorted_nations = sorted(df["Nation"].unique())
+nations = st.sidebar.multiselect(
+    "Nation",
+    options=sorted_nations,
+    default=sorted_nations
 )
 
-# Filtro de Categoria
-sorted_categories = sorted(df["Category"].unique())
-categories = st.sidebar.multiselect(
-    "Categoria",
-    options=sorted_categories,
-    default=sorted_categories
+# Filtro de Posições
+sorted_positions = sorted(df["Pos"].unique())
+positions = st.sidebar.multiselect(
+    "Pos",
+    options=sorted_positions,
+    default=sorted_positions
 )
 
-# Filtro de Ano
-years = sorted(df["Order Date"].dt.year.unique())
-year_range = st.sidebar.slider(
-    "Ano",
-    min_value=int(min(years)),
-    max_value=int(max(years)),
-    value=(int(min(years)), int(max(years)))
+# Filtro de Idade
+Ages = sorted(df["Age"].dt.year.unique())
+Age_range = st.sidebar.slider(
+    "Age",
+    min_value=int(min(Ages)),
+    max_value=int(max(Ages)),
+    value=(int(min(Ages)), int(max(Ages)))
 )
 
 # Aplicar filtros
 filtered_df = df[
-    (df["Region"].isin(regions)) &
-    (df["Category"].isin(categories)) &
-    (df["Order Date"].dt.year.between(year_range[0], year_range[1]))
+    (df["Nation"].isin(nations)) &
+    (df["Pos"].isin(positions)) &
+    (df["Age"].dt.year.between(Age_range[0], Age_range[1]))
 ]
 
 # --------------------------------------------------
 # Parte superior com KPIs
 # --------------------------------------------------
-total_sales = filtered_df["Sales"].sum()
-total_profit = filtered_df["Profit"].sum()
-num_orders = filtered_df["Order ID"].nunique()
+total_golos = filtered_df["Gls"].sum()
+total_Assistências = filtered_df["Ast"].sum()
+num_minutos = filtered_df["Min"].nunique()
 
 # Vamos dividir a área em 3 colunas para mostrar os KPIs lado a lado
 col1, col2, col3 = st.columns(3)
-col1.metric("💰 Vendas Totais", f"${total_sales:,.0f}")
-col2.metric("📈 Lucro Total", f"${total_profit:,.0f}")
-col3.metric("🧾 Nº de Encomendas", num_orders)
+col1.metric("⚽ Golos Totais", f"{total_golos:,.0f}")
+col2.metric("📈 Assistências Totais", f"{total_Assistências:,.0f}")
+col3.metric("⏱️ Nº de Minutos", f"{num_minutos:,.0f}")
 
 st.divider()
 
 # ------------------------------------------------------------------
-# Gráfico 1 - Vendas ao longo do tempo (cada categoria é uma série)
+# Gráfico 1 - Golos por idade (cada categoria é uma série)
 # ------------------------------------------------------------------
-st.subheader("📅 Vendas ao longo do tempo por categoria")
+st.subheader("📅 Golos por idade")
 
 # Agrupar a soma de Sales em cada mês por Category
-sales_over_time = (
+golos_por_idade = (
     filtered_df
     .groupby(
-        [pd.Grouper(key="Order Date", freq="ME"), "Category"]
-    )["Sales"]
+        [pd.Grouper(key="Age", freq="YE"), "Category"]
+    )["Gls"]
     .sum()
     .reset_index()
 )
 
 # Criar uma pivot table
-sales_pivot = sales_over_time.pivot(
-    index="Order Date",
+golos_pivot = golos_por_idade.pivot(
+    index="Age",
     columns="Category",
-    values="Sales"
+    values="Gls"
 )
 
-st.line_chart(sales_pivot)
+st.line_chart(golos_pivot)
 
 # --------------------------------------------------
-# Gráfico 2 - Vendas por Região
+# Gráfico 2 - Golos por Região
 # --------------------------------------------------
-st.subheader("🌍 Vendas por Região")
+st.subheader("🌍 Golos por Região")
 
-# Agrupar a soma de Sales por Region
-sales_by_region = (
+# Agrupar a soma de Golos por Region
+golos_by_region = (
     filtered_df
-    .groupby("Region")["Sales"]
+    .groupby("Region")["Gls"]
     .sum()
 )
 
-st.bar_chart(sales_by_region)
+st.bar_chart(golos_by_region)
 
 st.divider()
 
 # --------------------------------------------------
-# Table - Top produtos
+# Table - Top jogadores
 # --------------------------------------------------
-st.subheader("🏆 Top 10 produtos por vendas")
+st.subheader("🏆 Top 10 jogadores por Golos")
 
-# Agrupar a soma de Sales por Product Name, ordenar e mostrar os top 10
-top_products = (
+# Agrupar a soma de Golos por Player Name, ordenar e mostrar os top 10
+top_players = (
     filtered_df
-    .groupby("Product Name")["Sales"]
+    .groupby("Player Name")["Gls"]
     .sum()
     .sort_values(ascending=False)
     .head(10)
 )
 
-st.dataframe(top_products)
+st.dataframe(top_players)
 
 # --------------------------------------------------
 # Rodapé
 # --------------------------------------------------
-st.caption("Dados: Sample Superstore")
+st.caption("Dados: Keegle")
